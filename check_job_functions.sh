1996cd7aed1e27aeda61f9fc030766b53c4ffe03
ilobon <irene.lobon@crick.ac.uk>
2025-03-07 16:33:14 +0000

    updated aliases and functions

--- check_job_functions.sh
+++ check_job_functions.sh
@@ -6,44 +6,34 @@
 
 #### 1. Check jobs in the queue ####
 
-
-alias q='squeue -u $(id -un) --format="%.9i %22j %8u %10T %.10M %.13l %6D %20R %6C %11m %E"'
-
-function j() {
-      squeue --format='%.18i %.9P %.8j %.8u %.2t %.10M %.6D %.5C %.20R %.20L' -u $(id -un)
-}
-
-function jr(){
-        squeue --format='%.18i %.9P %.8j %.8u %.2t %.10M %.6D %.5C %.20R %.20L' -u $(id -un) | awk '{if($5=="R")print $0}'
-} 
-
-function jp(){
-        squeue --format='%.18i %.9P %.8j %.8u %.2t %.10M %.6D %.5C %.20R %.20L' -u $(id -un) | awk '{if($5=="PD")print $0}'
-}
-
-function qw() {
-        squeue --format='%10i %.100j %.2t %.10M %.6D %.5C %.20L %.9P %.20R %.150Z' -u $(id -un) | less -S
-}
+# Print info on all the user's jobs in the queue
+alias q='squeue -u $(id -un) --format="%.9i %26j %8u %10T %.9P %6D %20R %6C %11m %.10M %.13L"'
+# Print info on RUNNING jobs
+alias qr='squeue -u $(id -un) --format="%.9i %26j %8u %10T %.9P %6D %20R %6C %11m %.10M %.13L" | grep -w -v "PENDING"'
+# Print info on PENDING jobs
+alias qp='squeue -u $(id -un) --format="%.9i %26j %8u %10T %.9P %6D %20R %6C %11m %.10M %.13L" | grep -w -v "RUNNING"'
+# Print job info, focusing on the working directory
+alias qwd='squeue -u $(id -un) --format="%.9i %20j %8u %10T %.95Z"'
 
 
-#### 2. Retrieve info on a specific running or pending job ####
+#### 2. Retrieve info on a specific job ####
 
-runjobinfo(){
-scontrol show job $1
-}
-
-
-#### 3. Check info on a specific finished job ####
-
+# Print information on a specific job, including its steps and max memory used if it has finished (MaxRSS)
 jobinfo(){
-        sacct -j $1 --format="JobID%15,JobName%30,State%25,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,NodeList" --units=G
+        sacct -j $1 --format="JobID%15,JobName%30,State,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,NodeList" --units=G
 }
 
+# Print working directory a specific job
 jobwhere(){
-        sacct -j $1 --format="JobID%15,JobName%150,State%25,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,ConsumedEnergy,ReqNodes,AllocNodes,SubmitLine%300,WorkDir%250" --units=G
+        sacct -j $1 --format="JobID%15,JobName%20,State,WorkDir%100" --units=G
 }
 
-## Return easy to read usage for a finished job
+# Print working directory a specific job, prints 300 characters of the workdir instead of 100
+jobwherelong(){
+        sacct -j $1 --format="JobID%15,JobName%20,State,WorkDir%300" --units=G | less -S
+}
+
+## Return easy to read usage percentage for a finished job
 
 jobusage(){
         req=$(sacct -j $1 --format="ReqMem" --units=G | sed -n 3p | tr -d 'G' | tr -d ' ')
@@ -52,20 +42,41 @@ jobusage(){
         echo "Requested $req GB and used $used GB, a usage of $usage%"
 }
 
-
-#### 4. Find recently run jobs ####
-
-weekInfo(){
-        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%15,JobName%30,State%25,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,End" --units=G
-}
-
-ydayInfo(){
-        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%15,JobName%30,State%25,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,End" --units=G
-}
-
-ydayWorkdir(){
-        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%15,JobName%150,State%25,ReqMem,MaxRSS,Timelimit,Elapsed,ReqCPUS,AllocCPUS,End,SubmitLine%100,WorkDir%250" --units=G
+# Print long format information about a job - just runs scontrol show
+jobinfodump(){
+scontrol show job $1
 }
 
 
+#### 3. Find recently run jobs ####
+
+# Print information on jobs run in the last day
+ydayinfo(){
+        sacct --starttime $(date -d "$date -1 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,Start,End,Elapsed,Timelimit,ReqMem%6,MaxRSS%6,ReqCPUS%5,AllocCPUS%5" --units=G | grep -v ".extern"
+}
+
+# Print information on jobs run in the last week
+weekinfo(){
+        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,Start,End,Elapsed,Timelimit,ReqMem%6,MaxRSS%6,ReqCPUS%5,AllocCPUS%5" --units=G | grep -v ".extern"
+}
+
+# Print jobID, name and working directory of jobs run in the last day
+ydayinfowd(){
+        sacct --starttime $(date -d "$date -1 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,WorkDir%100" --units=G | grep -v ".extern" | grep -v ".batch"
+}
+
+# Print jobID, name and working directory of jobs run in the last day - for long workdirs
+ydayinfowdlong(){
+        sacct --starttime $(date -d "$date -1 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,WorkDir%300" --units=G | grep -v ".extern" | grep -v ".batch"
+}
+
+# Print jobID, name and working directory of jobs run in the last week
+ydayinfowd(){
+        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,WorkDir%100" --units=G | grep -v ".extern" | grep -v ".batch"
+}
+
+# Print jobID, name and working directory of jobs run in the last weel - for long workdirs
+ydayinfowdlong(){
+        sacct --starttime $(date -d "$date -7 days" +"%Y-%m-%d") --endtime $(date -d "$date +1 days" +"%Y-%m-%d") --format="JobID%9,State%9,JobName%30,WorkDir%300" --units=G | grep -v ".extern" | grep -v ".batch"
+}
 
